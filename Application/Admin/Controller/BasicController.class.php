e1a60385db2d4d48799a51ee32ae0844b5c25325
kiva12138 <sunhaoxx@foxmail.com>
2018-07-03 17:17:49 +0800

    修改

--- Application/Admin/Controller/BasicController.class.php
+++ Application/Admin/Controller/BasicController.class.php
@@ -17,17 +17,23 @@ class BasicController extends CommonController {
 
 	public function add() {
 		if($_POST) {
-			if(!$_POST['title']) {
-				return show(0, '站点信息不能为空');
+			if(!$_POST['username']) {
+				return show(0, '请输入名字');
 			}
-			if(!$_POST['keywords']) {
-				return show(0, '站点关键词');
+			if(!$_POST['password']) {
+				return show(0, '请输入密码');
 			}
-			if(!$_POST['description']) {
-				return show(0, '站点描述');
+			if(!$_POST['password2']) {
+				return show(0, '请输入密码');
 			}
-
-			D("Basic")->save($_POST);
+			if($_POST['password'] != $_POST['password2']){
+				return show(0, '两次的密码不一样');
+			}
+			$temp = D("Admin")->getAdminByUsername($_POST['username']);
+			if(!$temp){
+				return show(0, '找不到用户');
+			}
+			D("Admin")->updatePasswordByName($temp['admin_id'], $_POST['password']);
 			return show(1, '配置成功');
 		}else {
 			return show(0, '没有提交的数据');
